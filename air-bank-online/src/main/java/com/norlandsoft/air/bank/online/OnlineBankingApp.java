53dc5dbdfd48e7f1779626346307c788a54ae568
Eric Chai <eric.chai@outlook.com>
2025-07-04 11:28:04 +0800

    引入OpenFeign

--- air-bank-online/src/main/java/com/norlandsoft/air/bank/online/OnlineBankingApp.java
+++ air-bank-online/src/main/java/com/norlandsoft/air/bank/online/OnlineBankingApp.java
@@ -3,12 +3,14 @@ package com.norlandsoft.air.bank.online;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Created by ChaiMingXu, on 2025/7/3
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class OnlineBankingApp {
   public static void main(String[] args) {
     SpringApplication.run(OnlineBankingApp.class, args);
